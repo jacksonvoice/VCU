1557786cb792681ec341bf2e7f1ad61bcdd295c5
Daniel Jackson <jacksonvoice@yahoo.com>
2014-06-11 11:21:32 -0700

    updated database vids collection

--- client/videos.js
+++ client/videos.js
@@ -1,3 +1,7 @@
-   Template.videos.rendered = function () {
-   
-};
+Template.videos.rendered = function () {
+      jwplayer(this.data.divSelector).setup({
+         playlist    : this.data.url,
+         width       : this.data.width,
+         aspectratio : this.data.aspectratio
+      });
+   };
--- server/fixtures.js
+++ server/fixtures.js
@@ -1,8 +1,10 @@
 if (vids.find().count() === 0) {
 	vids.insert ({
-		_id: "1",
-		viddiv: '<div id=\'player64I7ulrK\'><\/div>',
-		vidscrip: "jwplayer(\'player64I7ulrK\').setup({\r\n        playlist: \'\/\/jwpsrv.com\/feed\/64I7ulrK.rss\',\r\n        width: \'100%\',\r\n        aspectratio: \'16:9\'\r\n    });"
+		_id:'1',
+		url:'//jwpsrv.com/feed/64I7ulrK.rss',
+   		width:'100%',
+        aspectratio:'16:9',
+        divSelector:'64I7ulrK'
 	});
 }
 
